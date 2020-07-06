5e66e64be92702fc734788762f9896462c9be1e1
zhouyuhua <zhouyuhua@aocyun.com>
2020-07-06 17:21:52 +0800

    commit 0.0.2 version

---
+++ 0.0.2/CCVideoSDK.framework/Headers/CCVideoSDK.h
@@ -0,0 +1,19 @@
+//
+//  CCVideoSDK.h
+//  CCVideoSDK
+//
+//  Created by zyh on 2020/7/6.
+//  Copyright © 2020 CC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CCToolBox.h"
+//! Project version number for CCVideoSDK.
+FOUNDATION_EXPORT double CCVideoSDKVersionNumber;
+
+//! Project version string for CCVideoSDK.
+FOUNDATION_EXPORT const unsigned char CCVideoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCVideoSDK/PublicHeader.h>
+
+
